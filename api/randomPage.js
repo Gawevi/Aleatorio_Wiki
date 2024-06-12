657f26f818fd9461e3465c742131682dcd7a1f4a
Gawevi <gbielwade@gmail.com>
2024-06-12 15:35:44 -0300

    Correções2

--- api/randomPage.js
+++ api/randomPage.js
@@ -1,35 +1,39 @@
-// api/randomPage.js
+// randomPage.js
 
-const categoria = 'Players';
-const wikiUrl = 'https://liquipedia.net/valorant';
-const apiUrl = `${wikiUrl}/api.php?action=query&list=categorymembers&cmtitle=Category:${categoria}&cmlimit=500&format=json`;
+// Função que retorna uma página aleatória
+export default function getRandomPage() {
+    const categoria = 'Players';
+    const wikiUrl = 'https://liquipedia.net/valorant';
+    const apiUrl = `${wikiUrl}/api.php?action=query&list=categorymembers&cmtitle=Category:${categoria}&cmlimit=500&format=json`;
 
-fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Erro ao acessar a API: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        if ('query' in data && 'categorymembers' in data.query) {
-            const paginas = data.query.categorymembers;
-
-            if (paginas.length > 0) {
-                const paginaAleatoria = Math.floor(Math.random() * paginas.length);
-                const tituloPagina = paginas[paginaAleatoria].title;
-                const urlPagina = `${wikiUrl}/${tituloPagina.replace(' ', '_')}`;
-
-                console.log(`Página aleatória da categoria '${categoria}':`);
-                console.log(`Título: ${tituloPagina}`);
-                console.log(`URL: ${urlPagina}`);
-            } else {
-                console.log("Nenhuma página encontrada na categoria.");
+    return fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro ao acessar a API: ${response.status}`);
             }
-        } else {
-            console.log("Resposta da API não contém as informações esperadas.");
-        }
-    })
-    .catch(error => {
-        console.error(`Erro ao acessar a API: ${error}`);
-    });
+            return response.json();
+        })
+        .then(data => {
+            if ('query' in data && 'categorymembers' in data.query) {
+                const paginas = data.query.categorymembers;
+
+                if (paginas.length > 0) {
+                    const paginaAleatoria = Math.floor(Math.random() * paginas.length);
+                    const tituloPagina = paginas[paginaAleatoria].title;
+                    const urlPagina = `${wikiUrl}/${tituloPagina.replace(' ', '_')}`;
+
+                    return {
+                        title: tituloPagina,
+                        url: urlPagina
+                    };
+                } else {
+                    throw new Error("Nenhuma página encontrada na categoria.");
+                }
+            } else {
+                throw new Error("Resposta da API não contém as informações esperadas.");
+            }
+        })
+        .catch(error => {
+            throw new Error(`Erro ao acessar a API: ${error}`);
+        });
+}
